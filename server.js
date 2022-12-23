5aaa6c6432bab204001b5e3d8bd21a06352e612f
Abhishek Sharma <abhi.asno1@gmail.com>
2022-12-23 18:43:12 +0530

    a minor bug fix

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, POST, DELETE, OPTIONS");
     res.header("Access-Control-Allow-Headers", "Content-Type");
+    req.header("Access-Control-Allow-Origin", "*");
+    req.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, POST, DELETE, OPTIONS");
+    req.header("Access-Control-Allow-Headers", "Content-Type");
     next();
   });
 
